433905d0341408df7c72a454a8c10e977c27be97
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-02-25 18:09:21 -0300

    Omit errors for a while

--- lib/dropper/payment/create/server.ex
+++ lib/dropper/payment/create/server.ex
@@ -2,8 +2,8 @@ defmodule Dropper.Payment.Create.Server do
   alias Dropper.Payment
   use GRPC.Server, service: Payment.Create.Service
 
-  @spec perform(Payment.CreateRequest.t, GRPC.Server.Stream.t) :: Payment.CreateResponse.t
-  def perform(_request, _stream) do
-    Payment.CreateResponse.new(success: true, id: "New", errors: [])
+  @spec payment(Payment.CreateRequest.t, GRPC.Server.Stream.t) :: Payment.CreateResponse.t
+  def payment(_request, _stream) do
+    Payment.CreateResponse.new(success: true, id: "New")
   end
 end
