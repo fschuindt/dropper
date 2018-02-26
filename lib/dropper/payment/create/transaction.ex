6b66022c13d8b24eaf22a9c8904ec51e8e948fe2
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-02-26 19:25:53 -0300

    Use reference id to build associations

--- lib/dropper/payment/create/transaction.ex
+++ lib/dropper/payment/create/transaction.ex
@@ -1,22 +1,21 @@
 defmodule Dropper.Payment.Create.Transaction do
-  # def create_payer(request) do
-  #   %Dropper.Payer{}
-  #   |> Dropper.Payer.changeset(payer_attributes(request))
-  #   |> Dropper.Repo.insert()
-  # end
+  def create_payer(request) do
+    %Dropper.Payer{}
+    |> Dropper.Payer.changeset(payer_attributes(request))
+    |> Dropper.Repo.insert()
+  end
 
-  # def create_payment(request, payer) do
-  #   %Dropper.Payment{}
-  #   |> Dropper.Payment.changeset(payment_attributes(request, payer))
-  #   |> Dropper.Repo.insert()
-  # end
+  def create_payment(request, payer) do
+    %Dropper.Payment{}
+    |> Dropper.Payment.changeset(payment_attributes(request, payer))
+    |> Dropper.Repo.insert()
+  end
 
-  # defp payer_attributes(request) do
-  #   payer = request.payer
-  #   %{name: name, email: email} = %{name: payer.name, email: payer.email}
-  # end
+  defp payer_attributes(request) do
+    %{name: _name, email: _email} = %{name: request.payer.name, email: request.payer.email}
+  end
 
-  # defp payment_attributes(request, payer) do
-  #   %{payer_id: payer_id, method: method} = %{payer_id: payer.id, method: request.method}
-  # end
+  defp payment_attributes(request, payer) do
+    %{payer_id: _payer_id, method: _method} = %{payer_id: payer.id, method: request.method}
+  end
 end
