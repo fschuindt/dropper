1bfcb594fa1f0f382b948cdc45fbf6a68b9cf2e8
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-02-26 19:23:35 -0300

    Set Ecto associations and changesets

--- lib/dropper/payer.ex
+++ lib/dropper/payer.ex
@@ -1,10 +1,16 @@
-defmodule EctoAssoc.Payer do
+defmodule Dropper.Payer do
   use Ecto.Schema
 
   schema "payers" do
     field :name, :string
     field :email, :string
 
-    has_many :payments, EctoAssoc.Payment, on_delete: :delete_all
+    has_many :payments, Dropper.Payment, on_delete: :delete_all
+  end
+
+  def changeset(payer, params \\ %{}) do
+    payer
+    |> Ecto.Changeset.cast(params, [:name, :email])
+    |> Ecto.Changeset.validate_required([:email])
   end
 end
--- lib/dropper/payment.ex
+++ lib/dropper/payment.ex
@@ -1,8 +1,14 @@
-defmodule EctoAssoc.Payment do
+defmodule Dropper.Payment do
   use Ecto.Schema
 
   schema "payments" do
     field :method, :integer
-    belongs_to :payer, EctoAssoc.Payer
+    belongs_to :payer, Dropper.Payer
+  end
+
+  def changeset(payment, params \\ %{}) do
+    payment
+    |> Ecto.Changeset.cast(params, [:payer_id, :method])
+    |> Ecto.Changeset.validate_required([:payer_id])
   end
 end
