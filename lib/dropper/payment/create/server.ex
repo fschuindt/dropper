65d34026d131b536b486bfdcf66c4c27fa8a80b2
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-02-26 19:30:23 -0300

    Build Payer and Payment over Transaction

--- lib/dropper/payment/create/server.ex
+++ lib/dropper/payment/create/server.ex
@@ -4,27 +4,27 @@ defmodule Dropper.Payment.Create.Server do
 
   @spec payment(Payment.CreateRequest.t, GRPC.Server.Stream.t) :: Payment.CreateResponse.t
   def payment(request, _stream) do
-  #   Payment.Create.Transaction.create_payer(request)
-  #   |> create_payment(request)
+    Payment.Create.Transaction.create_payer(request)
+    |> create_payment(request)
   end
 
-  # defp create_payment({:ok, payer}, request) do
-  #   Payment.Create.Transaction.create_payment(request, payer)
-  #   |> terminate(payer)
-  # end
+  defp create_payment({:ok, payer}, request) do
+    Payment.Create.Transaction.create_payment(request, payer)
+    |> finish(payer)
+  end
 
-  # defp create_payment({:error, _changeset}, _request) do
-  #   Payment.CreateResponse.new(success: false)
-  # end
+  defp create_payment({:error, _changeset}, _request) do
+    Payment.CreateResponse.new(success: false)
+  end
 
-  # defp terminate({:ok, payment}, payer) do
-  #   payer_data = "Payer: #{payer.name} <#{payer.email}>"
-  #   payment_data = "Method: #{payment.method}"
+  defp finish({:ok, payment}, payer) do
+    payer_data = "Payer: #{payer.name} <#{payer.email}>"
+    payment_data = "Method: #{payment.method}"
 
-  #   Payment.CreateResponse.new(success: true, id: "#{payer_data} | #{payment_data}")
-  # end
+    Payment.CreateResponse.new(success: true, id: "#{payer_data} | #{payment_data}")
+  end
 
-  # defp terminate({:error, _changeset}, _payer) do
-  #   Payment.CreateResponse.new(success: false)
-  # end
+  defp finish({:error, _changeset}, _payer) do
+    Payment.CreateResponse.new(success: false)
+  end
 end
