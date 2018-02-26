62d4835cb184996847ccdf8121c7ca1736eadc38
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-02-26 16:08:57 -0300

    Comment old Ecto interface

--- lib/dropper/payment/create/server.ex
+++ lib/dropper/payment/create/server.ex
@@ -1,30 +1,30 @@
 defmodule Dropper.Payment.Create.Server do
   alias Dropper.Payment
-  use GRPC.Server, service: Payment.Create.Service
+  # use GRPC.Server, service: Payment.Create.Service
 
-  @spec payment(Payment.CreateRequest.t, GRPC.Server.Stream.t) :: Payment.CreateResponse.t
-  def payment(request, _stream) do
-    Payment.Create.Transaction.create_payer(request)
-    |> create_payment(request)
-  end
+  # @spec payment(Payment.CreateRequest.t, GRPC.Server.Stream.t) :: Payment.CreateResponse.t
+  # def payment(request, _stream) do
+  #   Payment.Create.Transaction.create_payer(request)
+  #   |> create_payment(request)
+  # end
 
-  defp create_payment({:ok, payer}, request) do
-    Payment.Create.Transaction.create_payment(request, payer)
-    |> terminate(payer)
-  end
+  # defp create_payment({:ok, payer}, request) do
+  #   Payment.Create.Transaction.create_payment(request, payer)
+  #   |> terminate(payer)
+  # end
 
-  defp create_payment({:error, _changeset}, _request) do
-    Payment.CreateResponse.new(success: false)
-  end
+  # defp create_payment({:error, _changeset}, _request) do
+  #   Payment.CreateResponse.new(success: false)
+  # end
 
-  defp terminate({:ok, payment}, payer) do
-    payer_data = "Payer: #{payer.name} <#{payer.email}>"
-    payment_data = "Method: #{payment.method}"
+  # defp terminate({:ok, payment}, payer) do
+  #   payer_data = "Payer: #{payer.name} <#{payer.email}>"
+  #   payment_data = "Method: #{payment.method}"
 
-    Payment.CreateResponse.new(success: true, id: "#{payer_data} | #{payment_data}")
-  end
+  #   Payment.CreateResponse.new(success: true, id: "#{payer_data} | #{payment_data}")
+  # end
 
-  defp terminate({:error, _changeset}, _payer) do
-    Payment.CreateResponse.new(success: false)
-  end
+  # defp terminate({:error, _changeset}, _payer) do
+  #   Payment.CreateResponse.new(success: false)
+  # end
 end
