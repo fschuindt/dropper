de589b27b1de934485556ae9a68cd60e3f38e360
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-02-26 21:30:55 -0300

    Autostart gRPC server

--- config/config.exs
+++ config/config.exs
@@ -11,6 +11,8 @@ config :dropper, Dropper.Repo,
 
 config :dropper, ecto_repos: [Dropper.Repo]
 
+config :grpc, start_server: true
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
