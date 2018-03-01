10704ee2038d69ac07d7615d23b619d19ad9ab41
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-03-01 10:21:52 -0300

    Document Dropper.Payment.Create.Server

--- lib/dropper/payment/create/server.ex
+++ lib/dropper/payment/create/server.ex
@@ -2,6 +2,22 @@ defmodule Dropper.Payment.Create.Server do
   alias Dropper.Payment
   use GRPC.Server, service: Payment.Create.Service
 
+  @moduledoc """
+  A gRPC server implementation for the creation of `Dropper.Payer`
+  and `Dropper.Payment` Ecto related entities. See
+  `lib/dropper/payment/payment.pb.ex` and
+  `proto/payment.proto` files for the gRPC definition.
+  """
+
+  @doc """
+  Recieves the gRPC request and stream. Performs the
+  `Dropper.Payment.Create.Transaction` functions and replies with a
+  relative gRPC response.
+
+  As it implements the `GRPC.Server` almost like a behaviour, the
+  stream (which is unnecessary here) is ignored but present to
+  maintain the API function arity.
+  """
   @spec payment(Payment.CreateRequest.t(), GRPC.Server.Stream.t()) :: Payment.CreateResponse.t()
   def payment(request, _stream) do
     Payment.Create.Transaction.create_payer(request)
